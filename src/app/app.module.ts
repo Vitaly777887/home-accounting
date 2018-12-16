f0ffb1184852586b495bb1f2d3fc6dad5254f1e9
Vitaly <chuzhinov2015@gmail.com>
2018-12-16 05:51:40 +0300

    Fix nag

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {SystemModule} from './system/system.module';
 import {HttpClientModule} from '@angular/common/http';
 
 import {AppComponent} from './app.component';
--- src/app/system/shared/components/header/header.component.ts
+++ src/app/system/shared/components/header/header.component.ts
@@ -5,7 +5,7 @@ import { User } from '../../../../shared/models/user.model';
 import { AuthService } from '../../../../shared/services/auth.service';
 
 @Component({
-  selector: 'header',
+  selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
